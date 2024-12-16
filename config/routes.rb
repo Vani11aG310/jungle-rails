c799f83d7dc480609d77c0cef44e33c259ab67c5
Angelo <angvil02@gmail.com>
2024-12-16 09:03:37 -0800

    Additional routes and migration for user signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # Routes for signup.
+  get '/signup' => 'users#new'
+  post '/users' => 'user#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
